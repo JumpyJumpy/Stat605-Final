83be0499a3c092f26525e2da62cda5224f472045
Shubo Lin <slin268@wisc.edu>
2021-11-19 17:12:27 -0600

    updated

--- split.sh
+++ split.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 touch cars{0..9}.txt
 
-tail +2 used_cars_data.csv | { col_index=$(cat col_index.txt);
+tail +2 used_cars_data.csv | {
+col_index=$(cat col_index.txt)
 while read i; do
     n=$(echo $i | cut -d "," -f 12)
     n=${n: -1}
