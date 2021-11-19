3c09314d84c57671359251b7401e8f06c62e9b4a
Shubo Lin <slin268@wisc.edu>
2021-11-19 17:32:13 -0600

    updated

---
+++ code/split.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+touch cars{0..9}.txt
+
+tail +2 used_cars_data.csv | {
+    col_index=$(cat col_index.txt)
+    while read i; do
+        n=$(echo $i | cut -d "," -f 12)
+        n=${n: -1}
+        echo $i | cut -d "," -f $col_index >>cars${n}.txt
+    done
+}
