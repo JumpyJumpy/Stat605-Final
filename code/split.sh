30133cec9bf6c7590ac96afa147860d3816bc9dc
Shubo Lin <slin268@wisc.edu>
2021-12-01 13:41:09 -0600

    update

--- code/split.sh
+++ code/split.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-touch cars{0..9}.txt
 
 tail +2 used_cars_data.csv | {
     col_index=$(cat col_index.txt)
     while read i; do
         n=$(echo $i | cut -d "," -f 12)
         n=${n: -1}
-        echo $i | cut -d "," -f $col_index >>cars${n}.txt
+        if [[ $n =~ [0-9] ]]; then
+            echo $i | cut -d "," -f ${col_index} >>cars_random_${n}.txt
+        fi
     done
 }
