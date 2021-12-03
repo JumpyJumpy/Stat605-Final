3ede72025d6ae2d400aa9e3d46ad5d2e0a3e19da
Shubo Lin <slin268@wisc.edu>
2021-12-03 16:15:40 -0600

    Update pre_split.sh

--- code/pre_split.sh
+++ code/pre_split.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir log error output
 
-head -n 1 used_cars_data.csv >header.csv
+head -n 1 cars_0.csv >header.csv
 split -a 1 -d -n 10  --additional-suffix=.csv used_cars_data.csv "cars_"
