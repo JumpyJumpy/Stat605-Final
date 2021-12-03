db21e8d943053bb0228357f047c6755e5363b260
Shubo Lin <slin268@wisc.edu>
2021-12-03 16:31:37 -0600

    update

--- code/pre_split.sh
+++ code/pre_split.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir log error output
-
-head -n 1 used_cars_data.csv >header.csv
+col_index=$(cat col_index.txt)
+head -n 1 used_cars_data.csv | cut -d "," -f ${col_index} >header.csv
 split -a 1 -d -n 10  --additional-suffix=.csv used_cars_data.csv "cars_"
