c3bb7bccc0750aba62719df4e87dee5e5b4d9570
Shubo Lin <slin268@wisc.edu>
2021-12-03 12:25:00 -0600

    update

--- code/mlr_by_brand.R
+++ code/mlr_by_brand.R
@@ -10,9 +10,11 @@ header <- colnames(read.csv("header.csv"))
 dataset <- read.csv("brand/cars_BMW.csv", header = FALSE, quote = "", na.strings = c("", "--", "NA", "NaN"), stringsAsFactors = TRUE)
 colnames(dataset) <- header
 summary(dataset)
+dataset$combine_fuel_economy <- NULL
 
-mlr_by_brand <- lm(price ~. - make_name - dearler_zip, data = dataset)
-mlr_summary <- summary(mlr_by_brand)
+mlr_by_brand <- aov(price ~ . - make_name - dealer_zip, dataset)
+# mlr_by_brand <- lm(price ~. - make_name - dearler_zip, data = dataset)
+mlr_summary <- summary(mlr_by_brand)Å
 
 sink(paste0("mlr_", file, ".txt"))
 print(mlr_summary)
