175658415e136a001f914f0611e11e5b730cd7f7
slin268 <slin268@learn.chtc.wisc.edu>
2021-12-02 23:20:59 -0600

    update

--- code/split.sh
+++ code/split.sh
@@ -9,23 +9,23 @@ cat $1 | {
     while read i; do
         n=$(echo $i | cut -d "," -f 43)
         if [[ $n == "Hyundai" ]]; then
-            echo $i | cut -d "," -f ${col_index} >>cars_random_${n}.csv
+            echo $i | cut -d "," -f ${col_index} >>cars_${n}.csv
         fi
 
         if [[ $n == "Chevrolet" ]]; then
-            echo $i | cut -d "," -f ${col_index} >>cars_random_${n}.csv
+            echo $i | cut -d "," -f ${col_index} >>cars_${n}.csv
         fi
 
         if [[ $n == "Ford" ]]; then
-            echo $i | cut -d "," -f ${col_index} >>cars_random_${n}.csv
+            echo $i | cut -d "," -f ${col_index} >>cars_${n}.csv
         fi
 
         if [[ $n == "Jeep" ]]; then
-            echo $i | cut -d "," -f ${col_index} >>cars_random_${n}.csv
+            echo $i | cut -d "," -f ${col_index} >>cars_${n}.csv
         fi
         
         if [[ $n == "BMW" ]]; then
-            echo $i | cut -d "," -f ${col_index} >>cars_random_${n}.csv
+            echo $i | cut -d "," -f ${col_index} >>cars_${n}.csv
         fi
     done
 }
