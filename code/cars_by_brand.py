cd683ce1e392e5edb38126750a5b99d6610ee999
Shubo Lin <slin268@wisc.edu>
2021-12-05 16:15:10 -0600

    updated

---
+++ code/cars_by_brand.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+
+brands = ["Hyundai", "Chevrolet", "Ford", "Jeep", "BMW"]
+
+pd.set_option("display.max_columns", None)
+cars = pd.read_csv("cars_splited.csv", low_memory = False)
+cars = cars[cars["make_name"].isin(brands)]
+cars = cars.drop(["vehicle_damage_category", "is_certified", "combine_fuel_economy"], axis = 1)
+
+for brand in brands:
+    cars[cars["make_name"] == brand].to_csv(f"")
