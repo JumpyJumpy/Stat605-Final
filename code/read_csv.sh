7ca0f89d61a5a5bc6359a76e6659b91245113644
Shubo Lin <slin268@wisc.edu>
2021-12-05 15:43:19 -0600

    Update read_csv.sh

--- code/read_csv.sh
+++ code/read_csv.sh
@@ -3,5 +3,5 @@ mkdir log error output
 col_index=$(cat col_index.txt)
 head -n 1 used_cars_data.csv | cut -d "," -f ${col_index} >header.csv
 
-cat used_cars_data.csv | csvquote | cut -d "," -f ${col_index} >car_0.csv
+cat used_cars_data.csv | csvquote | cut -d "," -f ${col_index} >car_splited.csv
 
