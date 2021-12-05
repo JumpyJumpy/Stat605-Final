61802b889b1537a9c33f4d29744dbe86ef804234
Qiluzhou <66503559+qiluzhou@users.noreply.github.com>
2021-12-05 12:54:27 -0600

    Add files via upload
    solve problems of reading csv with comma in columns

---
+++ code/read_csv.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mkdir log error output
+col_index=$(cat col_index.txt)
+head -n 1 used_cars_data.csv | cut -d "," -f ${col_index} >header.csv
+
+cat used_cars_data.csv | csvquote | cut -d "," -f ${col_index} >car_0.csv
+
