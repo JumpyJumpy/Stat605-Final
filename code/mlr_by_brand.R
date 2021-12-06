8834380214b154869e6501ccce0f4d9e56d792ff
Shubo Lin <slin268@wisc.edu>
2021-12-05 18:43:19 -0600

    Update mlr_by_brand.R

--- code/mlr_by_brand.R
+++ code/mlr_by_brand.R
@@ -8,12 +8,14 @@ if (length(args) == 1) {
 
 
 brand <- c("Hyundai", "Chevrolet", "Ford", "Jeep", "BMW")
-dataset <- read.csv("brand/cars_BMW.csv", na.strings = c("", "--", "NA", "NaN"), stringsAsFactors = TRUE, row.names = 1)
+dataset <- read.csv("brand/cars_BMW.csv", na.strings = c("", "--", "NA", "NaN"), row.names = 1)
 year <- 2021 - dataset$year
 summary(dataset)
 
-
-mlr_by_brand <- aov(price ~ . - make_name - dealer_zip, data = dataset)
+write.csv(na.omit(dataset), "cars_nona.csv")
+dataset_nona <- read.csv("cars_nona.csv", stringsAsFactors = TRUE, row.names = 1)
+summary(dataset_nona)
+mlr_by_brand <- aov(price ~ . - make_name - dealer_zip, data = dataset_nona)
 # mlr_by_brand <- lm(price ~. - make_name - dearler_zip, data = dataset)
 mlr_summary <- summary(mlr_by_brand)
 
