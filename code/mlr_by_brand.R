c935855770f48d8e6246b83a638d6f8efcbcc697
Shubo Lin <slin268@wisc.edu>
2021-12-05 19:22:27 -0600

    update

--- code/mlr_by_brand.R
+++ code/mlr_by_brand.R
@@ -14,8 +14,14 @@ summary(dataset)
 
 write.csv(na.omit(dataset), "cars_nona.csv")
 dataset_nona <- read.csv("cars_nona.csv", stringsAsFactors = TRUE, row.names = 1)
+dataset_nona$make_name <- NULL
+dataset_nona$dealer_zip <- NULL
+dataset_nona$interior_color <- NULL
+dataset_nona$exterior_color <- NULL
 summary(dataset_nona)
-mlr_by_brand <- aov(price ~ . - make_name - dealer_zip, data = dataset_nona)
+
+mlr_by_brand <- stepwise(prirce ~., dataset_nona)
+# mlr_by_brand <- aov(price ~ ., data = dataset_nona)
 mlr_summary <- summary(mlr_by_brand)
 
 sink(paste0("mlr_", file, ".txt"))
