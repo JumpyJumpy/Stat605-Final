e496997a88655c58c3d7c79c224d1685f122b718
Shubo Lin <slin268@wisc.edu>
2021-12-13 14:51:40 -0600

    update

--- code/mlr_by_brand.R
+++ code/mlr_by_brand.R
@@ -6,30 +6,26 @@ if (length(args) == 1) {
     stop()
 }
 
-
-brand_csv <- dir("./brand", full.names = TRUE)
+brand <- gsub(pattern = ".*_(.*)\\.csv", x = file, replacement = "\\1")
 
 mlr_results <- list()
-for (dirs in brand_csv) {
-    dataset <- read.csv(dirs, na.strings = c("", "--", "NA", "NaN"), row.names = 1)
-    dataset$year <- 2021 - dataset$year
-    summary(dataset)
+dataset <- read.csv(file, na.strings = c("", "--", "NA", "NaN"), row.names = 1)
+dataset$year <- 2021 - dataset$year
+## summary(dataset)
 
-    write.csv(na.omit(dataset), "cars_nona.csv")
-    dataset_nona <- read.csv("cars_nona.csv", stringsAsFactors = TRUE, row.names = 1)
-    dataset_nona$make_name <- NULL
-    dataset_nona$dealer_zip <- NULL
-    dataset_nona$interior_color <- NULL
-    dataset_nona$exterior_color <- NULL
-    summary(dataset_nona)
+write.csv(na.omit(dataset), "cars_nona.csv")
+dataset_nona <- read.csv("cars_nona.csv", stringsAsFactors = TRUE, row.names = 1)
+dataset_nona$make_name <- NULL
+dataset_nona$dealer_zip <- NULL
+dataset_nona$interior_color <- NULL
+dataset_nona$exterior_color <- NULL
+## summary(dataset_nona)
 
-    #mlr_by_brand <- stepwise(prirce ~., dataset_nona)
-    mlr_by_brand <- aov(price ~ ., data = dataset_nona)
-    mlr_summary <- summary(mlr_by_brand)
-    mlr_results[[dirs]] <- mlr_summary
+#mlr_by_brand <- stepwise(prirce ~., dataset_nona)
+mlr_by_brand <- aov(price ~ ., data = dataset_nona)
+mlr_summary <- summary(mlr_by_brand)
 
-}
 
-sink("mlr_result.txt")
-print(mlr_results)
+sink(paste0("mlr_result_", brand, ".txt"))
+print(mlr_summary)
 sink()
